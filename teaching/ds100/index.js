db51f9c9c05833083d5852fbdc52bb84c65d324e
Raguvir Kunani <rkunani@berkeley.edu>
2020-09-14 00:01:34 -0700

    try console logging

--- teaching/ds100/index.js
+++ teaching/ds100/index.js
@@ -2,6 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  console.log("hello");
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
