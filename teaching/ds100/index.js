a4641fea99374b093cbccfadc62478ab9a02af66
Raguvir Kunani <rkunani@berkeley.edu>
2020-09-14 00:16:07 -0700

    why is panel null?

--- teaching/ds100/index.js
+++ teaching/ds100/index.js
@@ -6,7 +6,7 @@ for (i = 0; i < acc.length; i++) {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
-
+    console.log(this);
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     console.log(panel);
